16c059636447ed0793a1de375fe7998a4efafd50
Asil Kaan Bozcuoglu <asil@cs.uni-bremen.de>
2017-06-06 17:42:23 +0200

    the example for logging added

--- src/client/client_openease.cpp
+++ src/client/client_openease.cpp
@@ -23,6 +23,8 @@
 using json = rapidjson::Document;
 using namespace rosbridge2cpp;
 
+string task_id = "";
+
 void connection_error_handler(TransportError err) {
   if(err == TransportError ::CONNECTION_CLOSED)
     std::cout << "Connection closed - You should reinit ROSBridge" << std::endl;
@@ -47,7 +49,16 @@ json service_handler(const json &message){
   return response;
 }
 
-void messageCallback(json &message){
+void startTaskCallback(json &message){
+  string solution = message["values"]["solution"].GetString();
+  json j;
+  j.Parse(solution.c_str());
+  task_id = j["ActionInst"].GetString();
+  std::cout << "Message received: " << Helper::get_string_from_rapidjson(message) << std::endl;
+  std::cout << "Created Task: " << task_id << std::endl;
+}
+
+void ordinaryCallback(json &message){
   std::cout << "Message received: " << Helper::get_string_from_rapidjson(message) << std::endl;
 }
 
@@ -61,7 +72,7 @@ int main()
   ROSBridge ros(t);
   ros.Init("https://localhost", 443);
   //std::this_thread::sleep_for(std::chrono::milliseconds(15000));
-  ROSTopic test_topic(ros, "/test", "std_msgs/String");
+  /*ROSTopic test_topic(ros, "/test", "std_msgs/String");
   std::ostringstream oss;
 
   rapidjson::Document message;
@@ -71,19 +82,59 @@ int main()
   test_topic.Publish(message);
 
   FunVcrJSON cb1 = messageCallback;
-  FunVcrJSON cb1a = messageCallback;
+  FunVcrJSON cb1a = messageCallback;*/
 
-  //test_topic.Unsubscribe(messageCallback);
-
-  /*ROSService test_service_call(ros, "/json_prolog/simple_query", "json_prolog_msgs/AddTwoInts");
+  //waiting for json prolog server initilized in the user container
+  std::this_thread::sleep_for(std::chrono::seconds(10));
   
-  rapidjson::Document service_params;
-  service_params.SetObject();
-  service_params.AddMember("mode",1, service_params.GetAllocator());
-  service_params.AddMember("id","hgahg6", service_params.GetAllocator());
-  service_params.AddMember("query","member(A, [a, b, c])", service_params.GetAllocator());
 
-  test_service_call.CallService(service_params, messageCallback);*/
+  ROSService simple_query_service(ros, "/json_prolog/simple_query", "json_prolog_msgs/PrologQuery");
+  rapidjson::Document start_task_query_params;
+  start_task_query_params.SetObject();
+  start_task_query_params.AddMember("mode",1, start_task_query_params.GetAllocator());
+  start_task_query_params.AddMember("id","hgahg6", start_task_query_params.GetAllocator());
+  start_task_query_params.AddMember("query","cram_start_action(knowrob:'CRAMAction', 'DummyContext', 1492785072, PA, ActionInst)", start_task_query_params.GetAllocator());
+
+  simple_query_service.CallService(start_task_query_params, ordinaryCallback);
+
+  ROSService next_solution_service(ros, "/json_prolog/next_solution", "json_prolog_msgs/PrologNextSolution");
+  
+  rapidjson::Document next_solution_params;
+  next_solution_params.SetObject();
+  next_solution_params.AddMember("id","hgahg6", next_solution_params.GetAllocator());
+  next_solution_service.CallService(next_solution_params, startTaskCallback);
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  rapidjson::Document end_task_query_params;
+  end_task_query_params.SetObject();
+  end_task_query_params.AddMember("mode",1, end_task_query_params.GetAllocator());
+  end_task_query_params.AddMember("id","hgahg7", end_task_query_params.GetAllocator());
+  end_task_query_params.AddMember("query","cram_finish_action('" + task_id + "', 1492785075)", end_task_query_params.GetAllocator());
+
+  simple_query_service.CallService(end_task_query_params, ordinaryCallback);
+  
+  
+  rapidjson::Document next_solution_params2;
+  next_solution_params2.SetObject();
+  next_solution_params2.AddMember("id","hgahg7", next_solution_params2.GetAllocator());
+  next_solution_service.CallService(next_solution_params2, ordinaryCallback);
+  
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  rapidjson::Document save_query_params;
+  save_query_params.SetObject();
+  save_query_params.AddMember("mode",1, save_query_params.GetAllocator());
+  save_query_params.AddMember("id","hgahg8", save_query_params.GetAllocator());
+  save_query_params.AddMember("query","rdf_save('/home/ros/user_data/testxx.owl', [graph('LoggingGraph')])", save_query_params.GetAllocator());
+
+  simple_query_service.CallService(save_query_params, ordinaryCallback);
+
+  
+  rapidjson::Document next_solution_params3;
+  next_solution_params3.SetObject();
+  next_solution_params3.AddMember("id","hgahg8", next_solution_params3.GetAllocator());
+  next_solution_service.CallService(next_solution_params3, ordinaryCallback);
 
   /*ROSService test_service_handler(ros, "/meow", "rospy_tutorials/AddTwoInts");
   test_service_handler.Advertise(service_handler);*/
